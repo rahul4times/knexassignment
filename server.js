803e7b21685790a47c3eb795e52b4d8a5c84a7fa
rahul4times <ams@Rahuls-MacBook-Air.local>
2017-10-13 17:15:43 -0700

    Basic functions work!

---
+++ server.js
@@ -0,0 +1,129 @@
+const env = 'development';
+const config = require('./knexfile.js')[env];
+const knex = require('knex')(config);
+const express = require("express");
+const path = require("path");
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const port = process.env.PORT || 8000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+// User's list
+app.get('/users', function(req, res) {
+  knex('users').then((userList) => {
+    res.render('users', {myObject: userList});
+  })
+  .catch((err) => {
+    console.error(err)
+  });
+});
+
+// Getting to create page
+app.get('/create', function(req, res) {
+  knex('users').then((userList) => {
+    //console.log(userList)
+    //res.json(result)
+    res.render('create', {myObject: userList});
+  })
+  .catch((err) => {
+    console.error(err)
+  });
+});
+
+
+// Create new user
+app.post('/create', function(req, res) {
+  knex('users')
+    .insert(req.body, '*') //req.body brings whole data structure
+    .then((result) => {
+      console.log(result);
+      //res.sendStatus(200);
+      res.redirect('/users');
+    })
+
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(400);
+    });
+
+    // function(err){
+    //   if(err) throw err;
+    //     console.log('Updated!');
+    //   res.redirect('/users');
+    // });
+});
+
+// Edit Page
+app.get('/edit/:uid', function(req, res) {
+  knex('users')
+  .where('id', req.params.uid)
+  .then((usersList) => {
+
+    //console.log(usersList);
+    res.render('edit', {myObject: usersList[0]});
+
+  })
+  .catch((err) => {
+    console.error(err)
+  });
+});
+
+
+// Edit user
+app.post('/edit/:uid', function(req, res) {
+  knex('users')
+    .update(req.body)
+    .where('id', req.params.uid)
+    .then((result) => {
+      console.log(req.body);
+      res.redirect('/edit/' + req.params.uid);
+    })
+    .catch((err) => {
+      console.log("THIS ERROR");
+      console.error(err);
+      res.sendStatus(400);
+    });
+});
+
+// Delete one user
+app.get('/delete/:id', function(req, res) {
+  
+  knex('users')
+    .del()
+    .where('id', req.params.id)
+    .then((result) => {
+      console.log(result);
+      res.redirect('/users');
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(400);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Server message on terminal window
+app.listen(port, function() {
+  console.log('Howdy!', port);
+});
